3978ef74431df2ab3189cd217592731a9cb915b2
paulohddias <paulohddias@gmail.com>
2023-10-11 15:13:40 -0300

    assossiando id do componete na classe Java

--- AppListaCurso/app/src/main/java/devandroid/paulo/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/paulo/applistacurso/view/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 import devandroid.paulo.applistacurso.R;
 import devandroid.paulo.applistacurso.model.Pessoa;
@@ -11,6 +13,9 @@ import devandroid.paulo.applistacurso.model.Pessoa;
 public class MainActivity extends AppCompatActivity {
 
     Pessoa pessoa;
+    EditText editPrimeiroNome, editSobrenome, editNomeCurso, editTelefoneContato;
+    Button btnLimpar, btnSalvar, btnFinalizar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +26,20 @@ public class MainActivity extends AppCompatActivity {
         pessoa.setSobreNome("Dias");
         pessoa.setCursoDesejado("Android");
         pessoa.setTelefoneContato("12-99757353");
+
+        editPrimeiroNome = findViewById(R.id.editPrimeiroNome);
+        editSobrenome = findViewById(R.id.editSobrenome);
+        editNomeCurso = findViewById(R.id.editNomeCurso);
+        editTelefoneContato = findViewById(R.id.editTelefoneContato);
+
+        btnFinalizar = findViewById(R.id.btnFinalizar);
+        btnLimpar = findViewById(R.id.btnLimpar);
+        btnSalvar = findViewById(R.id.btnSalvar);
+
+        editPrimeiroNome.setText(pessoa.getPrimeiroNome());
+        editSobrenome.setText(pessoa.getSobreNome());
+        editNomeCurso.setText(pessoa.getCursoDesejado());
+        editTelefoneContato.setText(pessoa.getTelefoneContato());
 
         Log.i("POOAndroid", pessoa.toString());
     }
