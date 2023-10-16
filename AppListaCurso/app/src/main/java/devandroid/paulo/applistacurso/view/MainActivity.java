dc40f1d39d9c38ace4d6092639aa2a2c5479696f
paulohddias <paulohddias@gmail.com>
2023-10-16 12:40:01 -0300

    Implementando os botoes de limpar, salvar e fechar

--- AppListaCurso/app/src/main/java/devandroid/paulo/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/paulo/applistacurso/view/MainActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import devandroid.paulo.applistacurso.R;
 import devandroid.paulo.applistacurso.model.Pessoa;
@@ -22,10 +24,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         pessoa = new Pessoa();
-        pessoa.setPrimeiroNome("Paulo");
-        pessoa.setSobreNome("Dias");
-        pessoa.setCursoDesejado("Android");
-        pessoa.setTelefoneContato("12-99757353");
+        //pessoa.setPrimeiroNome("Paulo");
+        //pessoa.setSobreNome("Dias");
+        //pessoa.setCursoDesejado("Android");
+        //pessoa.setTelefoneContato("12-99757353");
 
         editPrimeiroNome = findViewById(R.id.editPrimeiroNome);
         editSobrenome = findViewById(R.id.editSobrenome);
@@ -40,6 +42,36 @@ public class MainActivity extends AppCompatActivity {
         editSobrenome.setText(pessoa.getSobreNome());
         editNomeCurso.setText(pessoa.getCursoDesejado());
         editTelefoneContato.setText(pessoa.getTelefoneContato());
+
+        btnLimpar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editPrimeiroNome.setText("");
+                editSobrenome.setText("");
+                editNomeCurso.setText("");
+                editTelefoneContato.setText("");
+            }
+        });
+
+        btnFinalizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Volte Sempre", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
+
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pessoa.setPrimeiroNome(editPrimeiroNome.getText().toString());
+                pessoa.setSobreNome(editSobrenome.getText().toString());
+                pessoa.setCursoDesejado(editNomeCurso.getText().toString());
+                pessoa.setTelefoneContato(editTelefoneContato.getText().toString());
+
+                Toast.makeText(MainActivity.this, "Salvo "+pessoa.toString(), Toast.LENGTH_LONG).show();
+            }
+        });
 
         Log.i("POOAndroid", pessoa.toString());
     }
