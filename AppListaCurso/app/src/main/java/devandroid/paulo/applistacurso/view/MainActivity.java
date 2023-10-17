db2a6403e01a8c7b5c1797283cab1d9cc881d4cb
paulohddias <paulohddias@gmail.com>
2023-10-17 15:49:15 -0300

    Limpar os dados do Shared Preferences

--- AppListaCurso/app/src/main/java/devandroid/paulo/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/paulo/applistacurso/view/MainActivity.java
@@ -17,7 +17,9 @@ import devandroid.paulo.applistacurso.model.Pessoa;
 public class MainActivity extends AppCompatActivity {
 
     SharedPreferences preferences;
+    SharedPreferences.Editor listaVip;
     public static final String NOME_PREFERENCES = "pref_listavip";
+
 
     PessoaController controller;
 
@@ -31,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         preferences = getSharedPreferences(NOME_PREFERENCES, 0);
-        SharedPreferences.Editor listaVip = preferences.edit();
+        listaVip = preferences.edit();
 
         controller = new PessoaController();
 
@@ -62,6 +64,9 @@ public class MainActivity extends AppCompatActivity {
                 editSobrenome.setText("");
                 editNomeCurso.setText("");
                 editTelefoneContato.setText("");
+
+                listaVip.clear();
+                listaVip.apply();
             }
         });
 
