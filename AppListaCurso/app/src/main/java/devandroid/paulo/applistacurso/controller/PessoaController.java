fe34c3aec3ed7797c4611a0d51215e6e1122046e
paulohddias <paulohddias@gmail.com>
2023-10-17 13:04:28 -0300

    Criação  SharedPreferences e limpando o controler

--- AppListaCurso/app/src/main/java/devandroid/paulo/applistacurso/controller/PessoaController.java
+++ AppListaCurso/app/src/main/java/devandroid/paulo/applistacurso/controller/PessoaController.java
@@ -6,11 +6,8 @@ import devandroid.paulo.applistacurso.model.Pessoa;
 
 public class PessoaController {
 
-    SharedPreferences preferences;
-    public static final String NOME_PREFERENCES = "pref_listavip";
     public void salvar(Pessoa pessoa) {
-        preferences = getSharedPreferences(NOME_PREFERENCES, 0);
-        SharedPreferences.Editor listaVip = preferences.edit();
+
 
     }
 }
